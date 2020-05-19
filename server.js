783362c9900c8c1f8f4c106386db93b83d4aa94c
Norbert Toth <ntoth1990@gmail.com>
2020-05-19 19:17:55 +0200

    test mongoose schema added

--- server.js
+++ server.js
@@ -19,6 +19,38 @@ mongoose
     console.log('connection is made');
   });
 
+const TourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Every tour must have a name'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Every tour must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: 3,
+  },
+});
+
+const Tour = new mongoose.model('Tours', TourSchema);
+
+const testTour = new Tour({
+  name: 'test tour 3',
+  price: 345,
+});
+
+testTour
+  .save()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
