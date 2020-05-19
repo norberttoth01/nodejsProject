2c517b83307d66860bf48e50299ca8a2c108f802
Norbert Toth <ntoth1990@gmail.com>
2020-05-19 18:29:25 +0200

    config.env & mongoose added

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const port = 3000;
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('connection is made');
+  });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`app is running on port ${port} `);
